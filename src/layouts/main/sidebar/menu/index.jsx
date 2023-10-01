455175310accf7e4cebcab32df05136c99e580e3
kobrak1 <burakkarhan657@gmail.com>
2023-10-01 23:23:09 +0300

    new button added

--- src/layouts/main/sidebar/menu/index.jsx
+++ src/layouts/main/sidebar/menu/index.jsx
@@ -29,6 +29,30 @@ const Menu = () => {
                             </div>
                         </div>
             )}
+            {({isActive}) => (
+                            <div className={classNames("p-3 rounded-full inline-flex items-center gap-5 hover:bg-[#eff3f41a] transition-colors", {'font-bold':isActive} )}>
+                            {!isActive && (
+                                <svg viewBox="0 0 24 24" width={26.25} height={26.25}>
+                                    <path 
+                                        fill="rgba(231,233,234,1.00)"
+                                        d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"
+                                    />
+                                </svg>
+                            )}
+                            {isActive && (
+                                <svg viewBox="0 0 24 24" width={26.25} height={26.25}>
+                                    <path
+                                        fill="#fff"
+                                        d="M12 1.696L.622 8.807l1.06 1.696L3 9.679V19.5C3 20.881 4.119 22 5.5 22h13c1.381 0 2.5-1.119 2.5-2.5V9.679l1.318.824 1.06-1.696L12 1.696zM12 16.5c-1.933 0-3.5-1.567-3.5-3.5s1.567-3.5 3.5-3.5 3.5 1.567 3.5 3.5-1.567 3.5-3.5 3.5z"
+                                    />
+                                </svg>
+                            )}
+
+                            <div className="pr-4 text-xl">
+                                Home
+                            </div>
+                        </div>
+            )}
             {/* <div className="p-3 rounded-full inline-flex items-center gap-5 hover:bg-[#eff3f41a] transition-colors">
                 <svg viewBox="0 0 24 24" width={26.25} height={26.25}>
                     <path 
