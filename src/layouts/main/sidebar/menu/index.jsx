f6905b0b9ca0ed53e7a8962848f805ccbba8f8d3
kobrak1 <burakkarhan657@gmail.com>
2023-10-01 23:21:03 +0300

    classnames library is added and applied on sidebar

--- src/layouts/main/sidebar/menu/index.jsx
+++ src/layouts/main/sidebar/menu/index.jsx
@@ -1,21 +1,35 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
+import classNames from "classnames"
 
 const Menu = () => {
   return (
     <nav className="mt-0.5 mb-1">
-        <Link to='/' className="py-1 block group">
-            <div className="p-3 rounded-full inline-flex items-center gap-5 hover:bg-[#eff3f41a] transition-colors">
-                <svg viewBox="0 0 24 24" width={26.25} height={26.25}>
-                    <path
-                        fill="rgba(231,233,234,1.00)"
-                        d="M12 1.696L.622 8.807l1.06 1.696L3 9.679V19.5C3 20.881 4.119 22 5.5 22h13c1.381 0 2.5-1.119 2.5-2.5V9.679l1.318.824 1.06-1.696L12 1.696zM12 16.5c-1.933 0-3.5-1.567-3.5-3.5s1.567-3.5 3.5-3.5 3.5 1.567 3.5 3.5-1.567 3.5-3.5 3.5z"
-                    />
-                </svg>
-                <div className="pr-4 text-xl">
-                    Home
-                </div>
-            </div>
-            <div className="p-3 rounded-full inline-flex items-center gap-5 hover:bg-[#eff3f41a] transition-colors">
+        <NavLink to='/' className="py-1 block group">
+            {({isActive}) => (
+                            <div className={classNames("p-3 rounded-full inline-flex items-center gap-5 hover:bg-[#eff3f41a] transition-colors", {'font-bold':isActive} )}>
+                            {!isActive && (
+                                <svg viewBox="0 0 24 24" width={26.25} height={26.25}>
+                                    <path 
+                                        fill="#e7e9ea"
+                                        d="M12 9c-2.209 0-4 1.791-4 4s1.791 4 4 4 4-1.791 4-4-1.791-4-4-4zm0 6c-1.105 0-2-.895-2-2s.895-2 2-2 2 .895 2 2-.895 2-2 2zm0-13.304L.622 8.807l1.06 1.696L3 9.679V19.5C3 20.881 4.119 22 5.5 22h13c1.381 0 2.5-1.119 2.5-2.5V9.679l1.318.824 1.06-1.696L12 1.696zM19 19.5c0 .276-.224.5-.5.5h-13c-.276 0-.5-.224-.5-.5V8.429l7-4.375 7 4.375V19.5z"
+                                    />
+                                </svg>
+                            )}
+                            {isActive && (
+                                <svg viewBox="0 0 24 24" width={26.25} height={26.25}>
+                                    <path
+                                        fill="#fff"
+                                        d="M12 1.696L.622 8.807l1.06 1.696L3 9.679V19.5C3 20.881 4.119 22 5.5 22h13c1.381 0 2.5-1.119 2.5-2.5V9.679l1.318.824 1.06-1.696L12 1.696zM12 16.5c-1.933 0-3.5-1.567-3.5-3.5s1.567-3.5 3.5-3.5 3.5 1.567 3.5 3.5-1.567 3.5-3.5 3.5z"
+                                    />
+                                </svg>
+                            )}
+
+                            <div className="pr-4 text-xl">
+                                Home
+                            </div>
+                        </div>
+            )}
+            {/* <div className="p-3 rounded-full inline-flex items-center gap-5 hover:bg-[#eff3f41a] transition-colors">
                 <svg viewBox="0 0 24 24" width={26.25} height={26.25}>
                     <path 
                         fill="rgba(231,233,234,1.00)"
@@ -26,7 +40,7 @@ const Menu = () => {
                     Explore
                 </div>
             </div>
-            <div className="p-3 rounded-full flex items-center gap-5">
+            <div className="p-3 rounded-full inline-flex items-center gap-5 hover:bg-[#eff3f41a] transition-colors">
                 <svg viewBox="0 0 24 24" width={26.25} height={26.25} >
                     <path 
                         fill="rgba(231,233,234,1.00)"
@@ -36,7 +50,7 @@ const Menu = () => {
                     Notifications
                 </div>
             </div>
-            <div className="p-3 rounded-full flex items-center gap-5">
+            <div className="p-3 rounded-full inline-flex items-center gap-5 hover:bg-[#eff3f41a] transition-colors">
                 <svg viewBox="0 0 24 24" width={26.25} height={26.25}>
                     <path 
                         fill="rgba(231,233,234,1.00)"
@@ -47,7 +61,7 @@ const Menu = () => {
                     Messages
                 </div>
             </div>
-            <div className="p-3 rounded-full flex items-center gap-5">
+            <div className="p-3 rounded-full inline-flex items-center gap-5 hover:bg-[#eff3f41a] transition-colors">
                 <svg viewBox="0 0 24 24" width={26.25} height={26.25} >
                     <path 
                         fill="rgba(231,233,234,1.00)"
@@ -58,7 +72,7 @@ const Menu = () => {
                     Lists
                 </div>
             </div>
-            <div className="p-3 rounded-full flex items-center gap-5">
+            <div className="p-3 rounded-full inline-flex items-center gap-5 hover:bg-[#eff3f41a] transition-colors">
                 <svg viewBox="0 0 24 24" width={26.25} height={26.25} >
                     <path
                         fill="rgba(231,233,234,1.00)"
@@ -69,7 +83,7 @@ const Menu = () => {
                     Communities
                 </div>
             </div>
-            <div className="p-3 rounded-full flex items-center gap-5">
+            <div className="p-3 rounded-full inline-flex items-center gap-5 hover:bg-[#eff3f41a] transition-colors">
                 <svg viewBox="0 0 24 24" width={26.25} height={26.25} >
                     <path 
                         fill="rgba(231,233,234,1.00)"
@@ -80,7 +94,7 @@ const Menu = () => {
                     Premium
                 </div>
             </div>
-            <div className="p-3 rounded-full flex items-center gap-5">
+            <div className="p-3 rounded-full inline-flex items-center gap-5 hover:bg-[#eff3f41a] transition-colors">
                 <svg viewBox="0 0 24 24" width={26.25} height={26.25} >
                     <path 
                         fill="rgba(231,233,234,1.00)"
@@ -91,7 +105,7 @@ const Menu = () => {
                     Profile
                 </div>
             </div>
-            <div className="p-3 rounded-full flex items-center gap-5">
+            <div className="p-3 rounded-full inline-flex items-center gap-5 hover:bg-[#eff3f41a] transition-colors">
                 <svg viewBox="0 0 24 24" width={26.25} height={26.25} >
                     <path 
                         fill="rgba(231,233,234,1.00)"
@@ -101,8 +115,8 @@ const Menu = () => {
                 <div className="pr-4 text-xl">
                     More
                 </div>
-            </div>
-        </Link>
+            </div> */}
+        </NavLink>
     </nav>
   )
 }
